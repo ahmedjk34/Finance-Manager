39b15c4c10e28580acad0a8857326c08ab75bed6
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-06-25 16:07:03 +0300

    added context for currency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,13 @@ import Balance from "./Components/Balance/Balance";
 import Cashflow from "./Components/Cashflow/Cashflow";
 import Wishlist from "./Components/Wishlist/Wishlist";
 import { Route, Routes } from "../node_modules/react-router-dom/dist/index";
-import { User } from "./Types";
+import { Currency, User } from "./Types";
 import axios from "../node_modules/axios/index";
 import "./App.css";
 import Loading from "./Components/Loading";
 
 type Props = {};
-
+export const currencyContext = React.createContext<Currency | null>(null);
 function App({}: Props) {
   const [user, setUser] = useState<User | null>(null);
   useState(() => {
@@ -22,20 +22,21 @@ function App({}: Props) {
       .then((response) => setUser(response.data[0]))
       .catch((e) => console.log(e));
   });
-
   return user ? (
     <>
-      <Sidebar
-        username={user?.username}
-        profilePicture={user?.profilePicture}
-      ></Sidebar>
-      <div className="main-overlay"></div>
-      <Routes>
-        <Route path="/" element={<Dashboard />}></Route>
-        <Route path="/balance" element={<Balance />}></Route>
-        <Route path="/cashflow" element={<Cashflow />}></Route>
-        <Route path="/wishlist" element={<Wishlist />}></Route>
-      </Routes>
+      <currencyContext.Provider value={user.mainCurrency}>
+        <Sidebar
+          username={user?.username}
+          profilePicture={user?.profilePicture}
+        ></Sidebar>
+        <div className="main-overlay"></div>
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/balance" element={<Balance />}></Route>
+          <Route path="/cashflow" element={<Cashflow />}></Route>
+          <Route path="/wishlist" element={<Wishlist />}></Route>
+        </Routes>
+      </currencyContext.Provider>
     </>
   ) : (
     <Loading />
