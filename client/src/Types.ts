0ba761a94b0dc6fd6076ff50d8be6231bfe5149d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-06-25 16:07:24 +0300

    added List type

--- client/src/Types.ts
+++ client/src/Types.ts
@@ -1,12 +1,17 @@
-export interface currency {
+export interface Currency {
   name: String;
   symbol: String;
+}
+export interface List {
+  amount: number;
+  category: String;
+  date: Date;
 }
 export interface User {
   username: String;
   password: String;
   profilePicture: String;
-  mainCurrency: currency;
+  mainCurrency: Currency;
   balance: Number;
   wishList: Object;
   incomeCatagories: [String];
@@ -15,6 +20,6 @@ export interface User {
   currentIncome: Number;
   currentExpenses: Number;
   expensesGoal: Number;
-  spendingList: Object;
-  expensesList: Object;
+  spendingList: List;
+  expensesList: List;
 }
