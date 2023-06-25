a813bb64ef5462e4b1015f30bd3a3072a4872716
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-06-25 11:46:21 +0300

    Balance in Dashboard

--- client/src/Components/Dashboard/Balance.tsx
+++ client/src/Components/Dashboard/Balance.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-type Props = {
-  balance: Number;
-  symbol: String;
-};
+type Props = {};
 
-function Balance({ balance, symbol }: Props) {
+function Balance() {
+  const [balance, setBalance] = useState<Number>(0);
+  const [symbol, setSymbol] = useState<String>("$");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/user/balance")
+      .then((response) => {
+        setBalance(response.data[0].balance);
+        setSymbol(response.data[0].mainCurrency.symbol);
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div>
       <h3>Balance</h3>
       <div className="balance">
         <h1>
-          {balance}.00
-          {symbol}
+          {balance}.00{symbol}
         </h1>
+
         <h4>More Info &gt;</h4>
       </div>
     </div>
--- client/src/Components/Dashboard/Dashboard.tsx
+++ client/src/Components/Dashboard/Dashboard.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { User } from "../../Types";
 import Balance from "./Balance";
+import Cashflow from "./Cashflow";
 
-type Props = {
-  user: User | null;
-};
+type Props = {};
 
-function Dashboard({ user }: Props) {
+function Dashboard() {
   //@ts-ignore
-  const { balance, mainCurrency } = user;
+
   return (
     <div className="dashboard">
       <h1 className="section-title">Dashboard</h1>
       <div className="main-content">
-        <Balance balance={balance} symbol={mainCurrency.symbol} />
+        <Balance />
         <div className="calendar"></div>
-        <div className="cashflow"></div>
+        <Cashflow></Cashflow>
         <div className="wishlist"></div>
       </div>
     </div>
