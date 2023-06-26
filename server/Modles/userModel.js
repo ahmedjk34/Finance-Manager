4def9013a49b95918fa8d3a28e2ebae3ce00400d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-06-26 10:58:14 +0300

    added wishlist data

--- server/Modles/userModel.js
+++ server/Modles/userModel.js
@@ -80,7 +80,6 @@ module.exports = mongoose.model("user", userSchema);
 //   symbol: "$",
 // };
 // const balance = 1250;
-// const wishList = {};
 // const incomeGoal = 4000.0;
 // const currentIncome = 3000.0;
 // const expensesGoal = 2000.0;
@@ -97,6 +96,32 @@ module.exports = mongoose.model("user", userSchema);
 //   { amount: 500.0, category: "Business", date: "19/6/2023" },
 //   { amount: 30.0, category: "Food", date: "18/6/2023" },
 //   { amount: 42.5, category: "Food", date: "20/6/2023" },
+// ];
+// const wishList = [
+//   {
+//     item: "A new Monitor",
+//     link: "https://www.amazon.co.uk/Thinlerain-Gaming-Monitor-1920x1080P-DisplayPort/dp/B08Q794YDT/ref=sr_1_1?keywords=pc+monitors+144hz&qid=1687765964&s=handmade&sprefix=pc+mon%2Chandmade%2C144&sr=1-1",
+//     price: 130,
+//     dateAdded: "25/5/2023",
+//   },
+//   {
+//     item: "Necklace",
+//     link: "https://www.ebay.com/itm/383387201899?var=651834197520&_trkparms=%26rpp_cid%3D63586a376831990443d04aba%26rpp_icid%3D63586a376831990443d04ab9",
+//     price: 9,
+//     dateAdded: "24/5/2023",
+//   },
+//   {
+//     item: "Perfume",
+//     link: "https://www.fragrancenet.com/perfume/calvin-klein/euphoria/eau-de-parfum#139842",
+//     price: 65,
+//     dateAdded: "21/4/2023",
+//   },
+//   {
+//     item: "Gaming Mechanical Keyboard",
+//     link: "https://www.amazon.co.uk/SteelSeries-Apex-TKL-Tenkeyless-Illumination/dp/B09FQB51PZ/ref=sr_1_3?keywords=gaming+keyboard&qid=1687766191&sprefix=gaming+ke%2Caps%2C160&sr=8-3",
+//     price: 35,
+//     dateAdded: "4/4/2023",
+//   },
 // ];
 // m.create({
 //   username: username,
