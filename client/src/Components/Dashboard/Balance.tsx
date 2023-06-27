1338de9a49e81f07cad902351c74534197a2ea3b
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-06-27 10:57:51 +0300

    added linking to balance from dashboard

--- client/src/Components/Dashboard/Balance.tsx
+++ client/src/Components/Dashboard/Balance.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { currencyContext } from "../../App";
 
 type Props = {};
@@ -7,6 +8,7 @@ type Props = {};
 function Balance() {
   const [balance, setBalance] = useState<Number>(0);
   const symbol = useContext(currencyContext)?.symbol;
+  const navigation = useNavigate();
 
   useEffect(() => {
     axios
@@ -25,7 +27,7 @@ function Balance() {
           {symbol}
         </h1>
 
-        <h4>More Info &gt;</h4>
+        <h4 onClick={() => navigation("/balance")}>More Info &gt;</h4>
       </div>
     </div>
   );
